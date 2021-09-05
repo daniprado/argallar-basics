22aece1ed8553a69076f906cfd77203582082c65
Dani Prado <dani@argallar.com>
2021-09-05 12:32:46 +0200

    Solved kernel 5.8 compatibility

--- .local/bin/modprobe_4g.zsh
+++ .local/bin/modprobe_4g.zsh
@@ -2,14 +2,12 @@
 emulate -LR zsh
 set -o errexit
 
-O_PWD=${PWD}
+(cd ${HOME}/tmp; \
+  [ -d "xmm7360_usb" ] || git clone --branch compat-5.8 git@github.com:Ecos-hj/xmm7360_usb.git)
 
-cd ${HOME}/tmp
-[ -d "xmm7360_usb" ] || git clone git@github.com:juhovh/xmm7360_usb.git 
+(cd ${HOME}/tmp/xmm7360_usb; \
+  make clean; make; sudo make install)
 
-cd xmm7360_usb
-make clean && make && sudo make install
 sudo modprobe xmm7360_usb
 
-cd ${O_PWD}
 exit 0
