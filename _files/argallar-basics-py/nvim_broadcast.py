f3dc1357e6f413c54f4475e1f62989e3de38408c
Daniel Prado <daniel.prado@eriks.com>
2021-09-06 13:38:15 +0200

    Bugfix while accessing XDG_TEMP_HOME envvar

--- _files/argallar-basics-py/nvim_broadcast.py
+++ _files/argallar-basics-py/nvim_broadcast.py
@@ -11,7 +11,7 @@ DEFAULT_SOCKET_PATH = '/tmp'
 @click.option('--sock', type=click.Path(exists=True, file_okay=False), help='path where scoket files are')
 @click.argument('cmd')
 def cli(sock, cmd):
-    socket_path = sock if sock else osenv.get('XDG_TMP_HOME', DEFAULT_SOCKET_PATH)
+    socket_path = sock if sock else osenv.get('XDG_TEMP_HOME', DEFAULT_SOCKET_PATH)
     for file in listdir(path.expanduser(socket_path)):
         if file.startswith('nvimsocket_'):
             nvim = attach('socket', path=path.join(socket_path, file))
